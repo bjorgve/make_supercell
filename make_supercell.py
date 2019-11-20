eed62667029304df3f68750456a6d56c820a237f
Magnar Bjørgve <magnbjor@gmail.com>
2019-11-20 17:18:32 +0100

    Add method that removes periodically redundant coorners
    Coorners that are included through periodisation of the super cell can now be removed

--- make_supercell.py
+++ make_supercell.py
@@ -1,7 +1,8 @@
-from itertools import chain
+from itertools import chain, product
+from copy import deepcopy
 
 
-def enlarge_super_cell(atoms, period):
+def make_supercell(atoms, period):
     '''Takes a list of atoms and the period in a unit cell then generates
        a larger super cell
 
@@ -32,13 +33,43 @@ def enlarge_super_cell(atoms, period):
                     new_atoms.append([x[i], y[j], z[k]])
         new_atoms = [new_atom + [type] for new_atom in new_atoms]
         super_cell.append(new_atoms)
-    return list(chain(*super_cell)), [period*3.0 for period in period]
+    return list(chain(*super_cell)), [period*2.0 for period in period]
 
+
+def find_atom_in_origin(cell):
+    raw_cell = deepcopy(cell)
+    for atom in raw_cell:
+        del atom[-1]
+
+    return raw_cell.index([0.0, 0.0, 0.0])
+
+
+def strip_coorners(super_cell, period):
+    '''Takes a super_cell and it's period, strips the coorners that will
+       be replicated by the peridicity
+
+    Parameters:
+        super_cell (list): Coordinates and atom types of contained in the super
+                           cell
+        period (list): 3D period of the super cell
+
+    Returns:
+        super_cell (list): Stripped down super cell
+    '''
+    origin = super_cell[find_atom_in_origin(super_cell)]
+    type = origin[:].pop()
+    coorners = [list(atom) + [type]
+                for atom in list(product([0.0, period[0]], repeat=3))]
+    coorners.remove([0.0, 0.0, 0.0, type])
+    for coorner in coorners:
+        super_cell.remove(coorner)
+
+    return super_cell
 
 
 if __name__ == "__main__":
-    atoms = [[1.0, 1.0, 1.0, 'Ne'], [0.0, 0.0, 0.0, 'H']]
+    atoms = [[0.5, 0.5, 0.5, 'Ne'], [0.0, 0.0, 0.0, 'H']]
     period = [1.0, 1.0, 1.0]
     atoms, period = enlarge_super_cell(atoms, period)
-    print(atoms)
-    print(period)
+    # print(atoms)
+    atoms = strip_coorners(atoms, period)
