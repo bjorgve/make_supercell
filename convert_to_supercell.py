699e3703c5b85efc16cea5471dd9b03de82e7d95
Magnar Bjørgve <magnbjor@gmail.com>
2019-11-22 11:33:09 +0100

    Move read/write geometry to a singe file

--- convert_to_supercell.py
+++ convert_to_supercell.py
@@ -1,7 +1,6 @@
 from make_supercell import (make_supercell, strip_external_points,
                             reduce_boundary_points)
-from write_geometry import write_geometry
-from read_geometry import read_geometry
+from geometry import read_geometry, write_geometry
 
 import argparse
 
@@ -14,7 +13,6 @@ args = parser.parse_args()
 
 atoms, periods = read_geometry(args.input)
 atoms, periods = make_supercell(atoms, periods)
-print(len(atoms))
 atoms = strip_external_points(atoms, periods)
 atoms = reduce_boundary_points(atoms, periods)
 write_geometry(atoms, periods, args.output)
